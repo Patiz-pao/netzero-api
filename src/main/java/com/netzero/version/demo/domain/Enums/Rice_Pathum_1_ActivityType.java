d2480892715fbecd1a54cfdb5396a207544d0a49
nonthawat wattanawiwatwach <devnot.w@gmail.com>
2024-12-26 03:45:50 +0700

    Changes name to Pathum 1

--- src/main/java/com/netzero/version/demo/domain/Enums/Rice_Pathum_1_ActivityType.java
+++ src/main/java/com/netzero/version/demo/domain/Enums/Rice_Pathum_1_ActivityType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import static com.netzero.version.demo.Util.Constants.*;
 @Getter
 
-public enum Rice_Pathum_ActivityType implements RiceActivityType {
+public enum Rice_Pathum_1_ActivityType implements RiceActivityType {
     STORE_ELECTRICITY("ช่วงเตรียมพื้นที่","ติดตั้งโซล่าเซลล์และกักเก็บปริมาณไฟฟ้า",0,3),
     PLOWING_FIRST("ไถดะ (รอบที่ 1)","ใช้รถไถนาไฟฟ้าไถดินลึก 15 - 20 ซม. เพื่อพลิกดินและทำลายวัชพืช หลังไถ ทิ้งดินไว้ให้วัชพืชย่อยสลาย",USE_TRACTOR,1),
     REST_SOIL("ช่วงพักดิน","ทิ้งไว้ 5-6 วัน เพื่อให้ดินปรับตัวและวัชพืชที่ตายย่อยสลาย",0,5),
@@ -48,7 +48,7 @@ public enum Rice_Pathum_ActivityType implements RiceActivityType {
     private final double baseElectricityRequired;
     private final int duration;
 
-    Rice_Pathum_ActivityType(String name, String description, double baseElectricityRequired, int duration) {
+    Rice_Pathum_1_ActivityType(String name, String description, double baseElectricityRequired, int duration) {
         this.name = name;
         this.description = description;
         this.baseElectricityRequired = baseElectricityRequired;
